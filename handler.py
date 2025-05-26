8af16a2867d2ed8568514bac1fcf6a5211984fcb
curlyfunk <dimitrifromsofia@gmail.com>
2025-05-26 17:18:50 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,8 +1,29 @@
 import runpod
+import torch
+from diffusers import StableDiffusionXLPipeline
+from PIL import Image
+from io import BytesIO
+import base64
+
+pipe = StableDiffusionXLPipeline.from_pretrained(
+    "stabilityai/sdxl-turbo",
+    torch_dtype=torch.float16,
+    variant="fp16"
+).to("cuda")
+
+def encode_base64_image(image):
+    buffer = BytesIO()
+    image.save(buffer, format="PNG")
+    return base64.b64encode(buffer.getvalue()).decode("utf-8")
 
 def handler(event):
-    print(">>> Получена заявка:", event)
-    return {"output": "Готово, брат!"}
+    prompt = event["input"].get("prompt", "a photo of a person")
+    image = pipe(
+        prompt=prompt,
+        num_inference_steps=4,
+        guidance_scale=0.0,
+        added_cond_kwargs={}  # това оправя проблема
+    ).images[0]
+    return {"output_image": encode_base64_image(image)}
 
-print(">>> Стартиране на runpod.handler")
 runpod.serverless.start({"handler": handler})
