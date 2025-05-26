cd8b856f4f74077cbabc332f31a7b3c82fd507bc
curlyfunk <dimitrifromsofia@gmail.com>
2025-05-26 17:22:03 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Създаване на кеш директория в прикачения volume
 mkdir -p /runpod-volume/.cache/huggingface
+
+# Пренасочване към директорията с handler.py
 cd /workspace/InstantID
+
+# Стартира handler-а
 python3 handler.py
